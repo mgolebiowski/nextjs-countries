0b31b56a0f18b23135da46b452ac68970917b41b
Michal Golebiowski <michal.golabek94@gmail.com>
2021-07-24 22:06:55 +0200

    fix americas filter

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -29,7 +29,7 @@ export default function SearchForm({ onSearch = () => {} }: SearchFormProps) {
       <select name="continent" id="continent" value={chosenContinent || ''} onChange={setSearchState}>
         <option value=''>--</option>
         <option value="Africa">Africa</option>
-        <option value="America">Americas</option>
+        <option value="Americas">Americas</option>
         <option value="Asia">Asia</option>
         <option value="Europe">Europe</option>
         <option value="Oceania">Oceania</option>
