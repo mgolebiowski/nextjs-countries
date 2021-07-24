d9574e4a26812f41dc57672c9a03afefcca53221
Michal Golebiowski <michal.golabek94@gmail.com>
2021-07-24 22:07:09 +0200

    Add some styling for CountryBox

--- components/country-box.tsx
+++ components/country-box.tsx
@@ -14,7 +14,7 @@ export interface ICountry {
 export default function CountryBox({ country }: { country: ICountry }) {
   return (
     <div className={styles.CountryBox}>
-      <Image alt={`flag of ${country.name}`} src={country.flag} layout='responsive' width={document.body.clientWidth} height={200} />
+      <Image alt={`flag of ${country.name}`} src={country.flag} layout='responsive' width={350} height={200} />
       <div className={styles.details}>
         <h3>{country.name}</h3>
         <p>Population: {new Intl.NumberFormat('en-US').format(country.population)}</p>
